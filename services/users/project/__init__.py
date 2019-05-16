ab7a4b26711948e45e11db6f4af28bfc1050b01e
jakubsielewicz <39191369+jakubsielewicz@users.noreply.github.com>
2019-05-16 10:59:17 +1000

    Revert "Revert "Docker setup""
    This reverts commit d9fbe52f7e4a2feaa454228bc7fcae41d183e039.

--- services/users/project/__init__.py
+++ services/users/project/__init__.py
@@ -4,6 +4,7 @@ import os
 import sys
 from flask import Flask, jsonify
 from flask_restful import Resource, Api
+from flask_sqlalchemy import SQLAlchemy
 
 # instantiate the app
 app = Flask(__name__)
@@ -14,6 +15,21 @@ api = Api(app)
 app_settings = os.getenv('APP_SETTINGS') 
 app.config.from_object(app_settings)     
 #print(app.config, file=sys.stderr)
+
+# instantiate the db
+db = SQLAlchemy(app)
+
+# model
+class User(db.Model):
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    username = db.Column(db.String(128), nullable=False)
+    email = db.Column(db.String(128), nullable=False)
+    active = db.Column(db.Boolean(), default=True, nullable=False)
+
+    def __init__(self, username, email):
+        self.username = username
+        self.email = email
 
 class UsersPing(Resource):
     def get(self):
